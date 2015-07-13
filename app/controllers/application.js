85812b1f0354d0271d865b017b85cd964a3f4366
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-12 22:44:09 -0700

    adding more examples

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
+let now = new Date();
+let yesterday = now.setDate(now.getDate() - 1);
+let threeDaysAgo = now.setDate(now.getDate() - 2);
+
 export default Ember.Controller.extend({
 	intl: Ember.inject.service(),
 	locales: Ember.A(['en-us', 'en-gb', 'fr-fr']),
@@ -7,5 +11,17 @@ export default Ember.Controller.extend({
 		changeLocale(localeName) {
 			this.set('intl.locale', localeName);
 		}
-	}
+	},
+	num: 0,
+	now: now,
+	yesterday: yesterday,
+	threeDaysAgo: threeDaysAgo,
+	incrementTime: Ember.on('init', function() {
+			setInterval(() => {
+					Ember.run(() => {
+							this.set('now', new Date());
+							this.incrementProperty('num');
+					});
+			}, 500);
+	})
 });
