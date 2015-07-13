7fbbde126e1d96737da5a6d220f81d525858cc30
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-13 00:38:26 -0700

    adding polyfill

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,27 +1,26 @@
 import Ember from 'ember';
 
-let now = new Date();
-let yesterday = now.setDate(now.getDate() - 1);
+let now          = new Date();
+let yesterday    = now.setDate(now.getDate() - 1);
 let threeDaysAgo = now.setDate(now.getDate() - 2);
 
 export default Ember.Controller.extend({
-	intl: Ember.inject.service(),
-	locales: Ember.A(['en-us', 'en-gb', 'fr-fr']),
-	actions: {
-		changeLocale(localeName) {
-			this.set('intl.locale', localeName);
-		}
-	},
-	num: 0,
-	now: now,
-	yesterday: yesterday,
-	threeDaysAgo: threeDaysAgo,
-	incrementTime: Ember.on('init', function() {
-			setInterval(() => {
-					Ember.run(() => {
-							this.set('now', new Date());
-							this.incrementProperty('num');
-					});
-			}, 500);
-	})
+  intl: Ember.inject.service(),
+  actions: {
+    changeLocale(localeName) {
+      this.set('intl.locale', localeName);
+    }
+  },
+  locales: Ember.A(['en-us', 'en-gb', 'fr-fr']),
+  num: 1000,
+  now: now,
+  yesterday: yesterday,
+  threeDaysAgo: threeDaysAgo,
+  incrementTime: Ember.on('init', function() {
+    Ember.run.later(this, () => {
+      this.set('now', new Date());
+      this.incrementProperty('num');
+      this.incrementTime();
+    }, 1000);
+  })
 });
